eec6465692e3ae257f6b261a9cd6ed0d2fb97bb6
Prathamesh98 <prathamesh98@localhost.localdomain>
2020-01-17 18:59:08 +0530

    created project with create react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Homepage from "./HomeComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
